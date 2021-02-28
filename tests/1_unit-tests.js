69e36b74c02a078e56dfa7454bde88ecba1043f4
Hunter Lacefield <lace1010@gmail.com>
2021-02-28 17:31:48 +0800

    Fix typo for tests

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -90,7 +90,7 @@ suite("Unit Tests", function () {
     });
     // #10
     test("#approximately", function () {
-      assert.approximately(weirdNumbers(0.5), 1, 2);
+      assert.approximately(weirdNumbers(0.5), 1, 0.4);
       assert.approximately(weirdNumbers(0.2), 1, 0.8);
     });
   });
